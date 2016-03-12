5832edf417223f60168df4961fb91853bfdddcd7
talksthetalk <talksthetalk@gmail.com>
2016-03-12 11:53:54 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions take a square invertible matrix as input and return the inverse as output
+## The makeCacheMatrix function takes a square invertible matrix as input
+##    and generates a list of three functions (get, setsolve, and getsolve)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x,a,b) {
+  #initialize m
+  m <- NULL
+  #define functions and return list
+  get <- function() matrix(x,a,b)
+  setsolve <- function(aa) m <<- aa
+  getsolve <- function() m
+  list(get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(y) {
+  #test for cached
+  bb <- y$getsolve()
+  if(!is.null(bb)) {
+    message("getting cached data")
+    return(bb)
+  }
+  #generate inverse if not cached, and cache
+  cc<-y$setsolve(solve(y$get()))
+  cc
 }
